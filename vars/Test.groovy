0aa8ba5326db8a2165c35316d8df4634badf936e
Mason Buchanan <masonwb@gmail.com>
2026-03-12 23:39:08 -0400

    test

--- vars/Test.groovy
+++ vars/Test.groovy
@@ -10,5 +10,7 @@ def call(Map config = [:]) {
     String color = [SUCCESS: 'good', FAILURE: 'danger', UNSTABLE: 'warning'].get(status, '#439FE0')
 
     // slackSend(channel: channel, color: color, message: message)
-    echo channel color message
+    echo channel
+    echo color
+    echo message
 }
