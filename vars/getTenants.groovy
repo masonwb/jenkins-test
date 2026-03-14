e03e79324c4dbc4897182603f7656906c298960c
Mason Buchanan <masonwb@gmail.com>
2026-03-13 22:30:17 -0400

    import

--- vars/getTenants.groovy
+++ vars/getTenants.groovy
@@ -1,11 +1,11 @@
-import groovy.json.JsonSlurper
-
 /**
  * notifySlack.groovy
  * Send a Slack message: notifySlack(status: 'SUCCESS', channel: '#builds')
  */
 def call() {
   return '''
+    import groovy.json.JsonSlurper
+
     def response = new URL("http://host.docker.internal:3000/api/v1/tenants").text
     def json = new JsonSlurper().parseText(response)
 
