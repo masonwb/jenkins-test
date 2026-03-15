4d8cdeaecc73d5053ffb82f431a439c7014757e6
Mason Buchanan <masonwb@gmail.com>
2026-03-15 00:20:12 -0400

    test

--- vars/environmentParam.groovy
+++ vars/environmentParam.groovy
@@ -8,9 +8,14 @@ def call(String secret) {
         classpath: [],
         sandbox: false,
         script: '''
+          import groovy.json.JsonSlurper
+
           if (!Tenant) return ["Select a tenant first"]
 
-          return [Tenant + " Dev", Tenant + " Test", Tenant + " Prod"]
+          def response = new URL("http://host.docker.internal:3000/api/v1/tenants/" + Tenant + "/projects").text
+          def json = new JsonSlurper().parseText(response)
+
+          return json.collect { it.name }
         '''
       ],
       fallbackScript: [
