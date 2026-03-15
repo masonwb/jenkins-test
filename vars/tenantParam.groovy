e5e06f7123383a234319aeb6869c9b8aa00771db
Mason Buchanan <masonwb@gmail.com>
2026-03-15 00:31:14 -0400

    secret

--- vars/tenantParam.groovy
+++ vars/tenantParam.groovy
@@ -6,14 +6,14 @@ def call(String secret) {
       script: [
         classpath: [],
         sandbox: false,
-        script: """
+        script: '''
           import groovy.json.JsonSlurper
 
-          def response = new URL("http://host.docker.internal:3000/api/v1/tenants?secret=${secret}").text
+          def response = new URL("http://host.docker.internal:3000/api/v1/tenants?secret=''' + secret + '''").text
           def json = new JsonSlurper().parseText(response)
 
           return json.collect { it.name }
-        """
+        '''
       ],
       fallbackScript: [
         classpath: [],
