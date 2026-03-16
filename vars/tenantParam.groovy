091347da03e509df63124c02436f002fdae09cbe
Mason Buchanan <masonwb@gmail.com>
2026-03-15 20:16:18 -0400

    import

--- vars/tenantParam.groovy
+++ vars/tenantParam.groovy
@@ -11,6 +11,7 @@ def call(String credentialId) {
           import com.cloudbees.plugins.credentials.CredentialsProvider
           import com.cloudbees.plugins.credentials.common.StandardCertificateCredentials
           import jenkins.model.Jenkins
+          javax.net.ssl.HttpsURLConnection
 
           def creds = CredentialsProvider.lookupCredentials(
               StandardCertificateCredentials,
