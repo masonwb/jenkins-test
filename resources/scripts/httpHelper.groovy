cde4b242d6bce4e9cc977080d7fd600f06f2ae79
Mason Buchanan <masonwb@gmail.com>
2026-03-16 18:45:14 -0400

    test

--- resources/scripts/httpHelper.groovy
+++ resources/scripts/httpHelper.groovy
@@ -1,5 +1,69 @@
-class HelloWorld {
-  static String hello() {
-    return "Hello World"
+import com.cloudbees.plugins.credentials.common.StandardCertificateCredentials
+import javax.net.ssl.HttpsURLConnection
+
+class HttpHelper {
+  StandardCertificateCredentials credentials
+
+  HttpHelper(StandardCertificateCredentials credentials) {
+    this.credentials = credentials
+  }
+
+  get(String url) {
+    def ks = this.credentials.keyStore
+    def password = this.credentials.password.plainText
+
+    def kmf = javax.net.ssl.KeyManagerFactory.getInstance(javax.net.ssl.KeyManagerFactory.defaultAlgorithm)
+    kmf.init(ks, password.toCharArray())
+
+    def trustAllCerts = [
+      checkClientTrusted: { chain, authType -> },
+      checkServerTrusted: { chain, authType -> },
+      getAcceptedIssuers: { [] as java.security.cert.X509Certificate[] }
+    ] as javax.net.ssl.X509TrustManager
+
+    def sslContext = javax.net.ssl.SSLContext.getInstance('TLS')
+    sslContext.init(kmf.keyManagers, [trustAllCerts] as javax.net.ssl.TrustManager[], null)
+
+    def currentUrl = url
+    def cookies = [:]
+
+    for (int i = 0; i < 10; i++) {
+      HttpsURLConnection conn = (HttpsURLConnection) new URL(currentUrl).openConnection()
+      conn.SSLSocketFactory = sslContext.socketFactory
+      conn.hostnameVerifier = { hostname, session -> true } as javax.net.ssl.HostnameVerifier
+      conn.instanceFollowRedirects = false
+
+      if (cookies) {
+        conn.setRequestProperty('Cookie', cookies.collect { k, v -> "$k=$v" }.join('; '))
+      }
+
+      try {
+        int status = conn.responseCode
+
+        conn.headerFields.each { header, values ->
+          if (header?.equalsIgnoreCase('Set-Cookie')) {
+            values.each { raw ->
+              def pair = raw.split(';')[0].trim()
+              def eq = pair.indexOf('=')
+              if (eq > 0) {
+                cookies[pair[0..<eq]] = eq + 1 < pair.length() ? pair[(eq+1)..-1] : ''
+              }
+            }
+          }
+        }
+
+        if (status in [301, 302, 303, 307, 308]) {
+          def location = conn.getHeaderField('Location')
+          currentUrl = location.startsWith('http') ? location : new URL(new URL(currentUrl), location).toString()
+          continue
+        }
+
+        return (status < 400 ? conn.inputStream : conn.errorStream)?.text
+      } finally {
+        conn.disconnect()
+      }
+    }
+
+    throw new Exception("Too many redirects for URL: ${url}")
   }
 }
--- vars/tenantParam.groovy
+++ vars/tenantParam.groovy
@@ -16,59 +16,10 @@ def call(String folder, String credentialId) {
           def credentialHelper = new CredentialHelper("''' + folder + '''", "''' + credentialId + '''")
           def creds = credentialHelper.getCredential()
 
-          def ks = creds.keyStore  // already a KeyStore, no need to load from bytes
-          def password = creds.password.plainText
+          def httpClient = new HttpHelper(creds)
+          def response = httpClient.get("https://localhost:8443/api/v1/tenants")
 
-          def kmf = javax.net.ssl.KeyManagerFactory.getInstance(javax.net.ssl.KeyManagerFactory.defaultAlgorithm)
-          kmf.init(ks, password.toCharArray())
-
-          def trustAllCerts = [
-            checkClientTrusted: { chain, authType -> },
-            checkServerTrusted: { chain, authType -> },
-            getAcceptedIssuers: { [] as java.security.cert.X509Certificate[] }
-          ] as javax.net.ssl.X509TrustManager
-
-          def sslContext = javax.net.ssl.SSLContext.getInstance('TLS')
-          sslContext.init(kmf.keyManagers, [trustAllCerts] as javax.net.ssl.TrustManager[], null)
-
-          def currentUrl = "https://localhost:8443/api/v1/tenants"
-          def cookies = [:]
-          def maxRedirects = 10
-          def result = null
-
-          maxRedirects.times {
-            if (result != null) return
-
-            HttpsURLConnection conn = (HttpsURLConnection) new URL(currentUrl).openConnection()
-            conn.SSLSocketFactory = sslContext.socketFactory
-            conn.hostnameVerifier = { hostname, session -> true } as javax.net.ssl.HostnameVerifier
-            conn.instanceFollowRedirects = false
-
-            if (cookies) conn.setRequestProperty('Cookie', cookies.collect { k, v -> "$k=$v" }.join('; '))
-
-            int status = conn.responseCode
-
-            conn.headerFields.each { header, values ->
-              if (header?.equalsIgnoreCase('Set-Cookie')) {
-                values.each { raw ->
-                  def pair = raw.split(';')[0].trim()
-                  def eq = pair.indexOf('=')
-                  if (eq > 0) cookies[pair[0..<eq]] = eq + 1 < pair.length() ? pair[(eq+1)..-1] : ''
-                }
-              }
-            }
-
-            if (status in [301, 302, 303, 307, 308]) {
-              def location = conn.getHeaderField('Location')
-              currentUrl = location.startsWith('http') ? location : new URL(new URL(currentUrl), location).toString()
-              return
-            }
-
-            def responseBody = (status < 400 ? conn.inputStream : conn.errorStream)?.text
-            result = new JsonSlurper().parseText(responseBody).collect { it.name }
-          }
-
-          return result
+          return new JsonSlurper().parseText(responseBody).collect { it.name }
         '''
       ]
       // fallbackScript: [
